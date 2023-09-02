422ca148f8a61bf150b1e848b6988b94baba692f
nihal <nihal.abedin.annon99@gmail.com>
2023-09-03 05:36:20 +0600

    test pagination

--- src/components/Table/Table.svelte
+++ src/components/Table/Table.svelte
@@ -1,34 +1,38 @@
 <script>
-	// import TableBody from './TableBody/TableBody.svelte';
-	// import TableHeader from './TableHeader.svelte';
+	//imports
 	import { countryDataStore } from '../../stores';
 	import TableRowV2 from './TableBodyV2/TableRowV2.svelte';
-
-	//imports
-
-	let countries;
+	import { afterUpdate } from 'svelte';
+	//
+	let PAGE_NO = 1;
+	let countries = [];
+	let currentCountries = [];
 	countryDataStore.subscribe((value) => (countries = value));
 
-	const tableHeader = [
-		'Flag',
-		'Name',
-		'Population',
-		'CICO',
-		'UN Member Status',
-		'Currencies',
-		'Languages'
-	];
+	let start = 0;
+	let end = 10;
+
+	const handleNext = () => {
+		PAGE_NO += 1;
+	};
+	const handlePrevious = () => {
+		if (PAGE_NO === 1) {
+			return;
+		}
+		PAGE_NO -= 1;
+	};
+	afterUpdate(() => {
+		const newPage = PAGE_NO;
+		start = (newPage - 1) * 10;
+		end = newPage * 10;
+		currentCountries = countries.slice(start, end);
+	});
 </script>
 
 <main class=" h-full">
-	<!-- <TableHeader />
-	<TableBody /> -->
 	<table class="bg-white w-full border-collapse cTable">
 		<thead class="border-t-2">
 			<tr class="text-left">
-				<!-- {#each tableHeader as th}
-					<th class="p-3">{th}</th>
-				{/each} -->
 				<th class="p-3">Flag</th>
 				<th class="p-3">Name</th>
 				<th class="p-3">Population</th>
@@ -39,11 +43,15 @@
 			</tr>
 		</thead>
 		<tbody>
-			{#each countries as country}
+			{#each currentCountries as country}
 				<TableRowV2 {country} />
 			{/each}
 		</tbody>
 	</table>
+	<p>Showing 10 results of {countries.length}</p>
+	<button on:click={handlePrevious}>Previous</button>
+	<span>{`<${PAGE_NO}>`}</span>
+	<button on:click={handleNext}>Next</button>
 </main>
 
 <style>
